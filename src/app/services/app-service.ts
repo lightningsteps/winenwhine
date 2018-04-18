fe0d44b82d904bd5802aa899e947010df87f8ec0
Petar <pxc8399@g.rit.edu>
2018-04-18 20:07:11 +0200

    add comment

--- src/app/services/app-service.ts
+++ src/app/services/app-service.ts
@@ -2,6 +2,8 @@ import { Observable } from 'rxjs/Observable';
 import { Http, Response, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Headers, RequestMethod } from '@angular/http';
+import { CommentData } from '../models/CommentData';
+import { Wine } from '../models/Wine';
 
 @Injectable()
 export class AppService {
@@ -11,5 +13,11 @@ export class AppService {
 
   getReviews(): Observable<any> {
     return this._http.get('http://demo5732405.mockable.io/getReviews');
+  }
+
+  sendComments(id: number, comment: CommentData): Observable<Response> {
+    const headers = new Headers({ 'ContentType': 'application/x-www-form-urlencoded'});
+
+    return this._http.post('http://NESTONESTO/' + id + '/comments', comment);
   }
 }
